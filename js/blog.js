5fcf2a81f699246159d31bc9ba4ed2e0ce75b9e2
Zarino Zappia <mail@zarino.co.uk>
2013-12-14 09:12:43 +0000

    change header background colour based on time of day

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,5 @@
+/* Subheading carousel */
+
 var subheadings = [
   'develops things',
   'designs things',
@@ -28,6 +30,52 @@ var random = function(array){
   return array[Math.floor(Math.random() * array.length)]
 }
 
+
+/* Header background changes according to time of day */
+
+var colours = {
+  0: '#00161f',
+  5: '#00161f',
+  11: '#022131',
+  16: '#022b5a',
+  21: '#5e5b96',
+  27: '#ed943d',
+  34: '#1b89cf',
+  45: '#26b9ff',
+  55: '#26b9ff',
+  64: '#2cacc4',
+  73: '#e8cc5e',
+  77: '#a9410e',
+  89: '#022131',
+  100: '#00161f'
+}
+
+var getColourForPercentage = function(percent){
+  var match = colours[0]
+  $.each(colours, function(startPercent, colour){
+    if(percent > startPercent){
+      match = colour
+    }
+  })
+  return match
+}
+
+var changeHeader = function(hexColour){
+  $('header').animate({backgroundColor: hexColour}, 5000)
+}
+
+var checkTime = function(){
+  var d = new Date()
+  var h = d.getHours()
+  var m = d.getMinutes()
+  var percent = ((h + (m / 60)) / 24) * 100
+  var colour = getColourForPercentage(percent)
+  changeHeader(colour)
+}
+
+
+/* Dom ready */
+
 $(function(){
   var $toggle = $('<span id="menu-toggle"><i></i><i></i><i></i></span>').on('click', function(){
     $('header nav').slideToggle()
@@ -35,6 +83,9 @@ $(function(){
   })
   $('header nav').hide()
   $('header h1').prepend($toggle)
-  
-  var timer = setInterval(changeSubheading, 20000)
+
+  window.headingTimer = setInterval(changeSubheading, 15000)
+  setTimeout(changeSubheading, 2000)
+  window.colourTimer = setInterval(checkTime, 5000)
+  checkTime()
 })
