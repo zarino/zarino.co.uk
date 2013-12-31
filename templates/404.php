b5d7f0230c640749848b1be52bf76599163c16b7
Zarino Zappia <mail@zarino.co.uk>
2013-12-31 12:45:05 +0000

    factor analytics code out into separate file

--- templates/404.php
+++ templates/404.php
@@ -46,13 +46,6 @@ if(!isset($posts)){
             <p>Find him on <a href="https://twitter.com/zarino">Twitter</a> <a href="https://github.com/zarino">Github</a> and <a href="http://uk.linkedin.com/in/zarino">LinkedIn</a></p>
         </div>
     </footer>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-18117917-1', 'zarino.co.uk');
-      ga('send', 'pageview');
-    </script>
+<?php include('analytics.php'); ?>
 </body>
 </html>
